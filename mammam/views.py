fabfa887f1d761844e556e67c0551508ceb085cf
thaohvm <43254779+thaohvm@users.noreply.github.com>
2021-03-03 21:07:44 -0800

    register html

--- mammam/views.py
+++ mammam/views.py
@@ -1,11 +1,43 @@
 from django.shortcuts import render
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest, JsonResponse
 from django.urls import reverse
+from django.shortcuts import render
+
+from .models import User
 
 # Create your views here.
 
 def index(request):
     return render(request, "mammam/index.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "network/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "network/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
 
 def register(request):
     if request.method == "POST":
