0c57cf72aeb70e5e24b12777a8f6c4a646c72d5d
thaohvm <43254779+thaohvm@users.noreply.github.com>
2021-03-06 23:12:31 -0800

    remove ingr step

--- mammam/static/mammam/create.js
+++ mammam/static/mammam/create.js
@@ -6,11 +6,18 @@ document.addEventListener('DOMContentLoaded', function () {
 function add_ingredient() {
     let tableIngredients = document.getElementById("table-ingredients");
     let newIngredient = tableIngredients.insertRow(-1);
-    newIngredient.innerHTML = '<tr><td><input type="text" class="form-control" placeholder="250gr flour"></td></tr>';
+    newIngredient.innerHTML = '<tr><td><input type="text" class="form-control" placeholder="250gr flour"></td><td><button type="button" class="btn btn-secondary" onclick="remove_row($(this))">Remove</button></td></tr>';
 }
 
 function add_step() {
     let tableSteps = document.getElementById("table-steps");
     let newStep = tableSteps.insertRow(-1);
-    newStep.innerHTML = '<tr><td><input type="text" class="form-control" placeholder="Mix flour and water until they thicken"></td></tr>';
+    newStep.innerHTML = '<tr><td><input id="first-step" type="text" class="form-control" placeholder="Mix flour and water until they thicken"></td><td><button type="button" class="btn btn-secondary" onclick="remove_row($(this))">Remove</button></td></tr>';
+}
+
+function remove_row(button) {
+    // Only remove row if there are more than one row
+    if (button.closest("table").prop("rows").length > 1) {
+        button.closest("tr").remove();
+    }
 }
