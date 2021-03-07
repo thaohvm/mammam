3373865e863a37ef1a2a29b4a9307a9490ac29df
thaohvm <43254779+thaohvm@users.noreply.github.com>
2021-03-06 22:19:12 -0800

    add ingr step

--- mammam/static/mammam/create.js
+++ mammam/static/mammam/create.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
+    document.getElementById("button-ingredients").onclick = add_ingredient;
+    document.getElementById("button-steps").onclick = add_step;
 });
+
+function add_ingredient() {
+    let tableIngredients = document.getElementById("table-ingredients");
+    let newIngredient = tableIngredients.insertRow(-1);
+    newIngredient.innerHTML = '<tr><td><input type="text" class="form-control" placeholder="250gr flour"></td></tr>';
+}
+
+function add_step() {
+    let tableSteps = document.getElementById("table-steps");
+    let newStep = tableSteps.insertRow(-1);
+    newStep.innerHTML = '<tr><td><input type="text" class="form-control" placeholder="Mix flour and water until they thicken"></td></tr>';
+}
