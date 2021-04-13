3425a68dec8033014f80f9c210f8332c57cc0248
thaohvm <43254779+thaohvm@users.noreply.github.com>
2021-04-12 22:17:41 -0700

    allowed_hosts

--- final_project/settings.py
+++ final_project/settings.py
@@ -25,13 +25,9 @@ SECRET_KEY = "#r!zlgpq*%#)0#1_0^r34uzy2qc%7uh27x4ccot(_gw$stje&6"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-<<<<<<< Updated upstream
-ALLOWED_HOSTS = ["127.0.0.1"]
-=======
 ALLOWED_HOSTS = [
     "127.0.0.1",
 ]
->>>>>>> Stashed changes
 
 
 # Application definition
